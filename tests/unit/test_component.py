187a8b3d9ad55590de27bfd30e1fef44685742ea
Sean Leavey <gpg@attackllama.com>
2018-05-20 10:19:48 +0200

    Use solid child class of Component for tests

--- tests/unit/test_component.py
+++ tests/unit/test_component.py
@@ -2,27 +2,13 @@
 
 from unittest import TestCase
 
-from circuit.components import Component
+from circuit.components import Component as ComponentBase
 
-def concreter(_class, *args, **kwargs):
-    """Creates a non-abstract version of an abstract class
+class Component(ComponentBase):
+    """Child class of abstract ComponentBase"""
     
-    See https://stackoverflow.com/a/9759329/2251982.
-    """
-
-    if not "__abstractmethods__" in _class.__dict__:
-        return _class
-
-    new_dict = _class.__dict__.copy()
-
-    for abstractmethod in _class.__abstractmethods__:
-        # replace each abstract method or property with an identity function
-        new_dict[abstractmethod] = lambda x, *args, **kwargs: (x, args, kwargs)
-
-    # create a new class, overriding the abstract base class
-    return type("dummy_concrete_%s" % _class.__name__, (_class,), new_dict)
-
-component_class = concreter(Component)
+    def equation(self):
+        return NotImplemented
 
 class ComponentTestCase(TestCase):
     def setUp(self):
@@ -30,28 +16,28 @@ class ComponentTestCase(TestCase):
 
     def test_name(self):
         name = "component_name"
-        component = component_class(name=name)
+        component = Component(name=name)
         self.assertEqual(name, component.name)
 
     def test_name_empty(self):
         # empty string
         name = ""
-        component = component_class(name=name)
+        component = Component(name=name)
         self.assertEqual(name, component.name)
 
         # null
         name = None
-        component = component_class(name=name)
+        component = Component(name=name)
         self.assertEqual(name, component.name)
 
     def test_nodes_empty(self):
         # empty list
         nodes = []
-        component = component_class(nodes=nodes)
+        component = Component(nodes=nodes)
         self.assertEqual(nodes, component.nodes)
 
         # null
         nodes = None
-        component = component_class(nodes=nodes)
+        component = Component(nodes=nodes)
         # default to empty list
         self.assertEqual([], component.nodes)
