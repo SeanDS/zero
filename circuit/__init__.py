8ba4295fc6ef63fed4ebc8a2bd7a47781600e5bf
Sean Leavey <gpg@attackllama.com>
2018-05-20 18:59:46 +0200

    Bump version

--- circuit/__init__.py
+++ circuit/__init__.py
@@ -7,7 +7,7 @@ logging.getLogger().addHandler(logging.NullHandler())
 # use default locale (required for number formatting in log warnings)
 locale.setlocale(locale.LC_ALL, "")
 
-__version__ = "0.2.13"
+__version__ = "0.3.0"
 # description must be one line
 DESCRIPTION = "Linear circuit simulator based on Gerhard Heinzel's LISO"
 PROGRAM = "circuit"
