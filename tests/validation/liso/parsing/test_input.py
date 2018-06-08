a107c0ba490d6f4f7e5d2be9106f02ac7184230d
Sean Leavey <gpg@attackllama.com>
2018-06-08 21:41:32 +0200

    Add input parser tests

--- tests/validation/liso/parsing/test_input.py
+++ tests/validation/liso/parsing/test_input.py
@@ -1,10 +1,44 @@
 import unittest
 
 from circuit.liso import LisoInputParser
+from circuit.components import Node
 
 class LisoInputParserTestCase(unittest.TestCase):
     def setUp(self):
         self.parser = LisoInputParser()
+
+    def test_resistor(self):
+        self.parser.parse("r r1 10k n1 n2")
+        r = self.parser.circuit.get_component("r1")
+        self.assertEqual(r.name, "r1")
+        self.assertEqual(r.resistance, 10e3)
+        self.assertEqual(r.node1, Node("n1"))
+        self.assertEqual(r.node2, Node("n2"))
+
+    def test_capacitor(self):
+        self.parser.parse("c c1 10n n1 n2")
+        c = self.parser.circuit.get_component("c1")
+        self.assertEqual(c.name, "c1")
+        self.assertEqual(c.capacitance, 10e-9)
+        self.assertEqual(c.node1, Node("n1"))
+        self.assertEqual(c.node2, Node("n2"))
+
+    def test_inductor(self):
+        self.parser.parse("l l1 10u n1 n2")
+        l = self.parser.circuit.get_component("l1")
+        self.assertEqual(l.name, "l1")
+        self.assertEqual(l.inductance, 10e-6)
+        self.assertEqual(l.node1, Node("n1"))
+        self.assertEqual(l.node2, Node("n2"))
+
+    def test_opamp(self):
+        self.parser.parse("op op1 OP00 n1 n2 n3")
+        op = self.parser.circuit.get_component("op1")
+        self.assertEqual(op.name, "op1")
+        self.assertEqual(op.model.lower(), "op00")
+        self.assertEqual(op.node1, Node("n1"))
+        self.assertEqual(op.node2, Node("n2"))
+        self.assertEqual(op.node3, Node("n3"))
 
     def test_component_invalid_type(self):
         kwargs = {"text": """
