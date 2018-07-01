5775cd43f9cfcb41f3fcd16ebdc286ff84a55b53
Sean Leavey <gpg@attackllama.com>
2018-07-01 23:42:05 +0200

    Expand test

--- tests/integration/liso/test_input_parser.py
+++ tests/integration/liso/test_input_parser.py
@@ -46,7 +46,7 @@ uinput nii 0
 uoutput no all
 """)
         self.parser.build()
-        # there should be 1 explicit output and 3 op-amp outputs
+        # there should be 8 outputs
         self.assertEqual(8, self.parser.n_tf_outputs)
 
     def test_output_allop(self):
@@ -87,6 +87,31 @@ ioutput r2
         self.assertRaisesRegex(LisoParserError,
                                r"output element 'r2' is not present in the circuit",
                                self.parser.solution)
+
+    def test_output_all(self):
+        self.parser.parse("""
+c c1 270.88486n nm2 no
+c c2 7.516u no3 np1
+c ca 1.484u nm2 ni
+op n1a OP27 np1 gnd no1
+op n2a OP27 gnd nm2 no
+op n3a OP27 gnd nm3 no3
+r ri2 2.6924361k nm3 no1
+r ri1 1k no3 nm3
+r r1 40.246121k no1 nm2
+r r2 2.0651912k np1 no
+r ra 115.16129k nm2 ni
+r rb 2.0154407k np1 ni
+r rq 22.9111k nm2 no
+r load 1k no gnd
+r rin 50 nii ni
+freq log 1 100 10
+uinput nii 0
+ioutput load all
+""")
+        self.parser.build()
+        # there should be 16 outputs (15 components above plus input component)
+        self.assertEqual(16, self.parser.n_tf_outputs)
 
     def test_output_allop(self):
         self.parser.parse("""
