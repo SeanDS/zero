c208209eddd3f53893b9da8f398c02677d9660ab
Sean Leavey <gpg@attackllama.com>
2018-07-04 23:09:54 +0200

    First working version

--- datasheet/__main__.py
+++ datasheet/__main__.py
@@ -12,10 +12,11 @@ from .request import DatasheetRequest
 LOGGER = logging.getLogger(__name__)
 
 class Parser(object):
-    def __init__(self, program, version, err_stream=sys.stderr):
+    def __init__(self, program, version, info_stream=sys.stdout, err_stream=sys.stderr):
         self.parser = None
         self.program = program
         self._version = version
+        self.info_stream = info_stream
         self.err_stream = err_stream
 
         self._build_parser()
@@ -26,8 +27,24 @@ class Parser(object):
         # version flag
         self.parser.add_argument("--version", action="version", version=self.version)
 
+        # verbose flag
+        self.parser.add_argument("-v", "--verbose", action="store_true",
+                                 help="enable verbose output")
+
         # datasheet search term
-        self.parser.add_argument("search_term", help="search term")
+        self.parser.add_argument("term", help="search term")
+
+        # force first result
+        self.parser.add_argument("-f", "--first", action="store_true",
+                                 help="display first part without prompt")
+
+        # download and display
+        self.parser.add_argument("-d", "--display", action="store_true",
+                                 help="download and display datasheet")
+
+        # no wildcards
+        self.parser.add_argument("-e", "--exact", action="store_true",
+                                 help="don't add wildcard characters around search term")
 
     def parse(self, args):
         if len(args) == 1:
@@ -40,13 +57,91 @@ class Parser(object):
         self.action(namespace)
 
     def action(self, namespace):
-        request = DatasheetRequest(namespace.search_term)
+        if namespace.verbose:
+            # turn on logging
+            logging_on()
+        
+        datasheets = DatasheetRequest(namespace.term, exact=namespace.exact)
+
+        self._handle_parts(datasheets, first=namespace.first, display=namespace.display)
+
+    def _handle_parts(self, datasheets, first=False, display=True):
+        if datasheets.n_parts == 0:
+            self.error("No parts found")
+        elif datasheets.n_parts == 1 or first:
+            # one datasheet
+            datasheet = datasheets.parts[0]
+
+            # show results directly
+            self.info(datasheet)
+            self._handle_part(datasheet, first=first, display=display)
+        else:
+            self.info("Found multiple parts:")
+            for index, part in enumerate(datasheets.parts, 1):
+                self.info("%d: %s" % (index, part))
+            
+            chosen_part_idx = 0
+            while chosen_part_idx <= 0 or chosen_part_idx > datasheets.n_parts:
+                try:
+                    chosen_part_idx = int(input("Enter part number: "))
+
+                    if chosen_part_idx <= 0 or chosen_part_idx > datasheets.n_parts:
+                        raise ValueError
+                except ValueError:
+                    self.error("invalid, try again", exit=False)
+
+            self._handle_part(datasheets.parts[chosen_part_idx - 1])
+
+    def _handle_part(self, part, first=False, display=True):
+        if part.n_datasheets == 0:
+            self.error("No datasheets found for '%s'" % part.mpn)
+        elif part.n_datasheets == 1 or first:
+            # show results directly
+            self.info(part)
+            self._handle_datasheet(part.datasheets[0], display=display)
+        else:
+            self.info("Found multiple datasheets:")
+            for index, datasheet in enumerate(part.datasheets, 1):
+                self.info("%d: %s" % (index, datasheet))
+            
+            chosen_datasheet_idx = 0
+            while chosen_datasheet_idx <= 0 or chosen_datasheet_idx > datasheet.n_datasheets:
+                try:
+                    chosen_datasheet_idx = int(input("Enter datasheet number: "))
+
+                    if chosen_datasheet_idx <= 0 or chosen_datasheet_idx > datasheet.n_datasheets:
+                        raise ValueError
+                except ValueError:
+                    self.error("invalid, try again", exit=False)
+
+            self._handle_datasheet(part.datasheets[chosen_datasheet_idx - 1])
+
+    def _handle_datasheet(self, datasheet, display=True):
+        self.info("Created: %s" % datasheet.created)
+        self.info("Pages: %d" % datasheet.n_pages)
+        self.info("URL: %s" % datasheet.url)
+
+        if display:
+            # download and display
+            datasheet.display()
 
     def print_help(self, exit=False):
         self.parser.print_help(self.err_stream)
 
         if exit:
             # exit with error code
+            sys.exit(1)
+
+    def info(self, msg, exit=False):
+        print(msg, file=self.info_stream)
+    
+        if exit:
+            sys.exit(0)
+
+    def error(self, msg, exit=True):
+        print(msg, file=self.err_stream)
+
+        if exit:
             sys.exit(1)
 
     @property
--- datasheet/request.py
+++ datasheet/request.py
@@ -1,14 +1,26 @@
+import os
+import sys
+import subprocess
 import requests
 import json
+import logging
+import tempfile
+import progressbar
+
+LOGGER = logging.getLogger("datasheet")
 
 class DatasheetRequest:
     """Datasheet request handler"""
     API_ENDPOINT = "https://octopart.com/api/v3/parts/match"
     API_KEY = "ebdc07fc"
-    def __init__(self, keyword):
+    def __init__(self, keyword, exact=False):
         self.keyword = keyword
+        self.exact = exact
+        self.parts = None
+
+        self._request()
     
-    def request(self):
+    def _request(self):
         """Request datasheet"""
         # build parameters
         params = {"include[]": "datasheets",
@@ -24,17 +36,145 @@ class DatasheetRequest:
         if response.status_code != 200:
             raise Exception(response)
         
-        if not response.headers["content-type"].contains("application/json"):
+        if "application/json" not in response.headers["content-type"]:
             raise Exception("unknown response content type")
     
-        print(response)
+        response_data = response.json()
+
+        # debug info
+        LOGGER.debug("request took %d ms", response_data["msec"])
+
+        if not "results" in response_data:
+            raise Exception("unexpected response")
+        
+        # first list item in results
+        results = response_data["results"][0]
+
+        # parts
+        parts = results["items"]
+
+        LOGGER.debug("%d %s found", len(parts), ["part", "parts"][len(parts) != 1])
+
+        # store parts
+        self._parse_parts(parts)
+
+    def _parse_parts(self, raw_parts):
+        """Parse parts"""
+        parts = []
+        for part in raw_parts:
+            parts.append(Part(part))
+        
+        self.parts = parts
 
     @property
     def search_query(self):
         """Search query JSON string"""
-        return json.dumps({"mpn": self.keyword})
+        keyword = self.keyword
+        if not self.exact:
+            keyword = "*%s*" % keyword
+        
+        return json.dumps([{"mpn": keyword}])
 
     @property
     def default_params(self):
         """Default parameters to include in every request"""
         return {"apikey": self.API_KEY}
+    
+    @property
+    def n_parts(self):
+        """Number of parts found"""
+        return len(self.parts)
+
+class Downloadable:
+    def __init__(self, info_stream=sys.stdout):
+        self.info_stream = info_stream
+
+    def download(self, url, progress=True):
+        if progress:
+            stream = self.info_stream
+        else:
+            # null file
+            stream = open(os.devnull, "w")
+
+        pbar = progressbar.ProgressBar(widgets=['Downloading: ',
+                                                progressbar.Percentage(),
+                                                progressbar.Bar(),
+                                                progressbar.ETA()],
+                                       max_value=100, fd=stream).start()
+        
+        # make request
+        request = requests.get(url, stream=True)
+        total_data_length = int(request.headers.get("content-length"))
+
+        data_length = 0
+
+        tmp = tempfile.NamedTemporaryFile(delete=False)
+
+        with open(tmp.name, "wb") as f:
+            for chunk in request.iter_content(chunk_size=128):
+                if chunk:
+                    f.write(chunk)
+                
+                    data_length += len(chunk)
+
+                    if data_length == total_data_length:
+                        fraction = 100
+                    else:
+                        fraction = 100 * data_length / total_data_length
+                
+                    pbar.update(fraction)
+        
+        return tmp.name
+
+class Part:
+    def __init__(self, part_info):
+        self.brand = None
+        self.brand_url = None
+        self.manufacturer = None
+        self.manufacturer_url = None
+        self.mpn = None
+        self.url = None
+        self.datasheets = None
+
+        self._parse(part_info)
+    
+    def _parse(self, part_info):
+        self.brand = part_info["brand"]["name"]
+        self.brand_url = part_info["brand"]["homepage_url"]
+        self.manufacturer = part_info["manufacturer"]["name"]
+        self.manufacturer_url = part_info["manufacturer"]["homepage_url"]
+        self.mpn = part_info["mpn"]
+        self.url = part_info["octopart_url"]
+        self.datasheets = [Datasheet(datasheet) for datasheet in part_info["datasheets"]]
+
+    @property
+    def n_datasheets(self):
+        return len(self.datasheets)
+
+    def __repr__(self):
+        return "{brand} / {manufacturer} {mpn}".format(**self.__dict__)
+
+class Datasheet(Downloadable):
+    def __init__(self, datasheet_data):
+        self.created = None
+        self.n_pages = None
+        self.url = None
+
+        self._parse(datasheet_data)
+
+        super().__init__()
+    
+    def _parse(self, datasheet_data):
+        self.created = datasheet_data["metadata"]["date_created"]
+        self.n_pages = int(datasheet_data["metadata"]["num_pages"])
+        self.url = datasheet_data["url"]
+
+    def display(self):
+        self._open_pdf(self.download(url=self.url))
+
+    def _open_pdf(self, filename):
+        if sys.platform == "win32":
+            os.startfile(filename)
+        else:
+            opener = "open" if sys.platform == "darwin" else "xdg-open"
+            subprocess.call([opener, filename])
