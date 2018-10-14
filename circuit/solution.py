e69c0832c4645d830613c41ce1f3e7c7bb5dcab7
Sean Leavey <gpg@attackllama.com>
2018-10-14 18:39:35 +0200

    Remove unused import

--- circuit/solution.py
+++ circuit/solution.py
@@ -1,7 +1,6 @@
 """Plotting functions for solutions to simulations"""
 
 import logging
-import collections
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MultipleLocator
