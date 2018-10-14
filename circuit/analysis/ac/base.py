bdb35b476649bdda1e05b4008002de5a8ef475ff
Sean Leavey <gpg@attackllama.com>
2018-10-14 18:46:34 +0200

    Add debug message for prescaling and ensure empty node source/sink fields exist in constructor

--- circuit/analysis/ac/base.py
+++ circuit/analysis/ac/base.py
@@ -31,6 +31,8 @@ class BaseAcAnalysis(BaseAnalysis, metaclass=abc.ABCMeta):
 
         # empty fields
         self._solution = None
+        self._node_sources = None
+        self._node_sinks = None
 
         # validate the circuit for the current analysis
         self.validate_circuit()
@@ -196,6 +198,9 @@ class BaseAcAnalysis(BaseAnalysis, metaclass=abc.ABCMeta):
 
         # right hand side to solve against
         rhs = self.right_hand_side()
+
+        # debug info
+        LOGGER.debug("Matrix prescaling: %s", self.prescale)
 
         # frequency loop
         for index, frequency in enumerate(freq_gen):
