4af7e421bdcb2f4c3540d9e5e2a412deba5c86ec
Sean Leavey <gpg@attackllama.com>
2019-07-01 21:13:25 +0200

    Add response multiplication tests

--- tests/integration/test_data_function_operators.py
+++ tests/integration/test_data_function_operators.py
@@ -87,25 +87,42 @@ class FunctionMathematicalOperationUnitsTestCase(ZeroDataTestCase):
 
 class FunctionMathematicalOperationDataTestCase(ZeroDataTestCase):
     """Function mathematical operation data tests."""
-    def _check_response_multiply_right(self, response, scale):
+    def _check_response_multiply_scalar_right(self, response, scale):
         scaled_response = response * scale
         self.assertTrue(np.allclose(scaled_response.complex_magnitude,
                                     response.complex_magnitude * scale))
 
-    def _check_response_multiply_left(self, response, scale):
+    def _check_response_multiply_scalar_left(self, response, scale):
         scaled_response = scale * response
         self.assertTrue(np.allclose(scaled_response.complex_magnitude,
                                     scale * response.complex_magnitude))
 
-    def _check_response_multiply_both(self, response, scale):
-        self._check_response_multiply_left(response, scale)
-        self._check_response_multiply_right(response, scale)
+    def _check_response_multiply_scalar(self, response, scale):
+        self._check_response_multiply_scalar_left(response, scale)
+        self._check_response_multiply_scalar_right(response, scale)
+
+    def _check_response_multiply_response(self, left, right):
+        scaled = left * right
+        self.assertTrue(np.allclose(scaled.complex_magnitude,
+                                    left.complex_magnitude * right.complex_magnitude))
 
     def test_multiply_response_by_scalar(self):
-        """Test units when multiplying response by response."""
+        """Test data when multiplying response by scalar."""
         f = self._freqs()
         for scalar in (0, 1, 1.5, -2.3, 1e2, -1e2, 6.7e-10, 3.78e10+4.802e9j):
-            self._check_response_multiply_both(self._v_v_response(f), scalar)
-            self._check_response_multiply_both(self._v_i_response(f), scalar)
-            self._check_response_multiply_both(self._i_i_response(f), scalar)
-            self._check_response_multiply_both(self._i_v_response(f), scalar)
+            self._check_response_multiply_scalar(self._v_v_response(f), scalar)
+            self._check_response_multiply_scalar(self._v_i_response(f), scalar)
+            self._check_response_multiply_scalar(self._i_i_response(f), scalar)
+            self._check_response_multiply_scalar(self._i_v_response(f), scalar)
+
+    def test_multiply_response_by_response(self):
+        """Test data when multiplying response by response."""
+        f = self._freqs()
+        self._check_response_multiply_response(self._v_v_response(f), self._v_v_response(f))
+        self._check_response_multiply_response(self._v_v_response(f), self._v_i_response(f))
+        self._check_response_multiply_response(self._v_i_response(f), self._i_i_response(f))
+        self._check_response_multiply_response(self._v_i_response(f), self._i_v_response(f))
+        self._check_response_multiply_response(self._i_i_response(f), self._i_i_response(f))
+        self._check_response_multiply_response(self._i_i_response(f), self._i_v_response(f))
+        self._check_response_multiply_response(self._i_v_response(f), self._v_v_response(f))
+        self._check_response_multiply_response(self._i_v_response(f), self._v_i_response(f))
